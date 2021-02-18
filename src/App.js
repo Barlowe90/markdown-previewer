52350afeb7e3f88b845a09fec4f091d19290039a
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-02-18 19:54:19 +0100

    base
    App ready for coding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    setText({ text });
+  });
+
   return (
     <div className="text-center mt-5">
-      <textarea id="editor" className="" rows="6" cols="50"></textarea>
+      <textarea
+        id="editor"
+        className=""
+        rows="6"
+        cols="50"
+        autoFocus
+        onClick={() => setText(text)}
+      >
+        {text}
+      </textarea>
 
-      <div id="preview"></div>
+      <div id="preview-text">
+        <textarea id="preview" className="" readOnly rows="25" cols="100">
+          {setText}
+        </textarea>
+      </div>
     </div>
   );
 }
